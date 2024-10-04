a5b7344eedf3bf48dc1c4ac4d3e822d5d2236eb2
Andy <obrezkov.andy@gmail.com>
2024-10-04 22:04:59 +0300

    Add interface and functionality

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var mainText: UILabel!
+    let buttonPhrases = ["Получилось!", "Ура!", "Кнопка нажата", "Ещё раз!", "✅", "Круто!", "Swift?"]
+    var count = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        var buttonText: String?
+        repeat {
+            buttonText = buttonPhrases.randomElement()
+        } while buttonText == mainText.text
+                    
+        mainText.text = buttonText
+        startButton.setTitle("Вы нажали \(count) раз", for: .normal)
+        count += 1
+    }
+    
 }
 
